930f865496054e539a08c2f852e431556cfa03f5
Kaloyan Krastev <kaloyansen@gmail.com>
2025-03-14 04:13:58 +0200

    run

--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env sh
 
-
-export FLASK_WORK="/yocto/moineau"
+export FLASK_WORK_DIRECTORY="/yocto/moineau"
+export FLASK_TITLE="vtabec.tv"
 
 export IP_BAN_LIST_COUNT="3"
-export IP_BAN_LIST_SECONDS="10000"
+export IP_BAN_LIST_SECONDS="10000000"
 
 export FLASK_SECRET_KEY="68c80c893465f24b2a2151cd9bf9b49c3b9889823bca1a26cabf350864705429"
-export FLASK_LOG_FILE="$FLASK_WORK/log/log"
+export FLASK_LOG_FILE="$FLASK_WORK_DIRECTORY/log/log"
 
 echo initializing virtual envirenment
-source $FLASK_WORK/venv/bin/activate
+source $FLASK_WORK_DIRECTORY/venv/bin/activate
+
 echo starting application
 echo logging file $FLASK_LOG_FILE
-exec $FLASK_WORK/cam2web
-echo have a nice day
+exec $FLASK_WORK_DIRECTORY/cam2web
+
+echo
+
+
 
