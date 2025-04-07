4f16fcc93092ec81d1ccb9a2faa22a56f27e114b
Kaloyan Krastev <kaloyansen@gmail.com>
2025-04-07 11:40:11 +0300

    run

--- src/cam2web.py
+++ src/cam2web.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # real-time analysis and streaming video 
 # code by Kaloyan Krastev kaloyansen@gmail.com
-# music composed, orchestrated and conducted by Kaloyan Krastev 
 # title by Milko Ginev nmrp@abv.bg
+# music composed, orchestrated and conducted by Kaloyan Krastev 
 
 from gevent import monkey
 monkey.patch_all()
@@ -29,6 +29,7 @@ import select
 import logging
 import random
 import lorem
+from itertools import product
 
 from pygments import highlight
 from pygments.lexers import PythonLexer
@@ -44,12 +45,14 @@ class SecureContext:
         self.work_directory =     self.safe("FLASK_WORK_DIRECTORY")
         self.page_title     =     self.safe("PAGE_TITLE")
         self.secret_key     =     self.safe("FLASK_SECRET_KEY")
+        self.audio_dir      =     self.safe("AUDIO_DIRECTORY")
         self.fps_limit      = int(self.safe("FPS_LIMIT"))
         self.jpeg_quality   = int(self.safe("JPEG_QUALITY"))
         self.gevent_workers = int(self.safe("GEVENT_WORKERS"))
         self.flask_port     = int(self.safe("FLASK_PORT"))
         self.ban_count      = int(self.safe("IP_BAN_LIST_COUNT"))
         self.ban_seconds    = int(self.safe("IP_BAN_LIST_SECONDS"))
+        self.save_rand_min  = int(self.safe("SAVE_RANDOM_MINUTES"))
     def safe(self, var: str):
         """ let it be safe """
         good = os.getenv(var, 0)
@@ -64,38 +67,51 @@ class SecureContext:
 
 class InterThreadCommunication:
     """ shared data """
-    def __init__(self, font_name, font_size, frame_size):
+    def __init__(self, font, font_size, frame_size, save_random):
 
-        self.font_name = font_name
+        self.font = font
         self.font_size = font_size
         self.frame_size = frame_size
+        self.save_random = save_random
         self.raw = None
         self.frame = None
         self.running = True
-        self.fps_value = 0
+        self.fps_value = 10.0
+        self.count = 0
         self.count9 = 0
+        self.pos = 0
+        self.neg = 0
         self.wheel_index = 0
         self.wheel_state = ['-', '/', '|', '\\']
-        self.count = 0
-        self.save_random_event = 11111
         self.new_message()
-    def new_message(self, message = 0, persist = 108, speed = 6):
+    def new_message(self, message = 0, persist = 216, speed = 6):
 
         self.speed = speed
         self.persist = persist
-        self.x = frame_size_x
+        self.x = frame_size[0]
         if message: self.text = message
-        else: self.text = lorem.sentence()[:37].rstrip('.') + '!'
-        self.size = cv2.getTextSize(self.text, self.font_name, self.font_size, 1)[0]
+        else: self.text = lorem.sentence()[:37].rstrip('.').lower()
+        self.size = self.get_size(self.text) #  = cv2.getTextSize(self.text, self.font, self.font_size, 1)[0]
+    def get_size(self, text: str) -> float:
+
+        return cv2.getTextSize(text, self.font, self.font_size, 1)[0]
     def new_frame(self) -> bool:
 
         self.count += 1
         self.count9 = (self.count9 + 1) % 9
-        if self.x / self.size[0] + 1 < 0: self.x = self.frame_size - self.size[0] # not too complicated
+        if self.x / self.size[0] + 1 < 0: self.x = self.frame_size[0] - self.size[0] # not too complicated
         self.x -= self.speed
         if self.count % self.persist == 0: self.new_message()
         self.wheel_index = (self.wheel_index + 1) % len(self.wheel_state)
-        return self.count % self.save_random_event == 0
+        return self.count % self.save_random == 0
+    def dump(self):
+        """ print all """
+        print('\n', '=' * 16, 'shared data', '=' * 16)
+        for key, value in self.__dict__.items():
+
+            if key == 'raw' or key == 'frame': continue
+            print(key, value)
+        print('=' * 44, '\n')
     def wheel(self) -> str:
 
         return self.wheel_state[self.wheel_index]
@@ -106,8 +122,11 @@ sc = SecureContext(166)
 server = Flask(__name__, template_folder = f"{sc.work_directory}/static/template")
 server.secret_key = sc.secret_key
 
-os.makedirs(f"{sc.work_directory}/dataset/positives", exist_ok = True)
-os.makedirs(f"{sc.work_directory}/dataset/negatives", exist_ok = True)
+
+positives = f"{sc.work_directory}/dataset/positives"
+negatives = f"{sc.work_directory}/dataset/negatives"
+os.makedirs(positives, exist_ok = True)
+os.makedirs(negatives, exist_ok = True)
 os.makedirs(f"{sc.work_directory}/ban", exist_ok = True)
 
 cascade = [cv2.CascadeClassifier(f"{sc.work_directory}/dataset/classifier/cascade.xml"),
@@ -122,19 +141,12 @@ ip_ban.init_app(server)
 ip_ban.load_allowed()
 ip_ban.load_nuisances()
 
-frame_size_x = 640
-frame_size_y = 480
-frame_size_x = 320
-frame_size_y = 240
-simplex = cv2.FONT_HERSHEY_SIMPLEX
+#frame_size = (640, 480)
+frame_size = (320, 240)
 
 sleeping = 1e-2
 
-AUDIO_LOC = '/static/audio'
-AUDIO_DIR = f"{sc.work_directory}/static/audio"
-FONT_SIZE = 0.4
-
-itc = InterThreadCommunication(simplex, FONT_SIZE, frame_size_x)
+itc = InterThreadCommunication(cv2.FONT_HERSHEY_DUPLEX, 0.4, frame_size, sc.fps_limit * sc.save_rand_min * 60)
 bs_lock = BoundedSemaphore()
 client_set = set()
 
@@ -148,12 +160,8 @@ def is_not_ascii(mot: str) -> bool:
 
     for c in mot:
 
-        if 0 <= ord(c) <= 127:
-
-            continue
-        else:
-
-            return True
+        if 0 <= ord(c) <= 127: continue
+        else: return True
     return False
 
 
@@ -169,76 +177,90 @@ def keyboard_listener():
                 itc.running = False
             elif key == 'n':
                 # save a negative frame
-                save_frame("negatives")
+                save_frame('-')
             elif key == 'p':
                 # save a positive frame
-                save_frame("positives")
+                save_frame('+')
             elif key == 'r':
                 # reset command-line interfase
                 os.system('reset')
+            elif key == 'D':
+                # dump shared data
+                itc.dump()
+            elif key == 'd':
+                # dump context
+                sc.dump()
         gevent.sleep(0.1)  # Yield control back to gevent
 
 
-def save_frame(label: str, message = ''):
+def save_frame(label: str, message = '') -> int:
 
-    filename = f"dataset/{label}/{int(time.time())}"
-    filepath = f"{sc.work_directory}/{filename}{message}.jpg"
-    print(f"save {message} {filepath}")
+    if label == '+': save_in = positives
+    elif label == '-': save_in = negatives
+    else:
+
+        print(f"error in save_frame: unexpected label {label}")
+        itc.running = False
+        return 1
+    now = int(time.time())
+    filepath = f"{save_in}/{now}{message}.jpg"
     cv2.imwrite(filepath, itc.raw)
-    itc.new_message(f" {filename}")
+    itc.pos = len(os.listdir(positives))
+    itc.neg = len(os.listdir(negatives))
+    show = f"{label * 3} (+{itc.pos}, -{itc.neg}) {message}"
+    print(f"{show} {filepath}")
+    itc.new_message(f"{show} {now}")
     print("standby")
+    return 0
 
 
-def contrast(fr, text, position, font_scale):
+def outline(fr: np.ndarray, text: str, position: tuple):
 
-    color = (22, 22, 22)
-    outcolor = (222, 222, 222)
-    thickness = 1
-    outline = 1 #  cv2.LINE_AA
-    cv2.putText(fr, text, (position[0] - 1, position[1] - 1), simplex, font_scale, outcolor, outline, cv2.LINE_AA)
-    cv2.putText(fr, text, (position[0] + 1, position[1] - 1), simplex, font_scale, outcolor, outline, cv2.LINE_AA)
-    cv2.putText(fr, text, (position[0] - 1, position[1] + 1), simplex, font_scale, outcolor, outline, cv2.LINE_AA)
-    cv2.putText(fr, text, (position[0] + 1, position[1] + 1), simplex, font_scale, outcolor, outline, cv2.LINE_AA)
-    cv2.putText(fr, text, position, simplex, font_scale, color, thickness, cv2.LINE_AA)
+    for x, y in product([-1, 1], repeat = 2):
+
+        cv2.putText(fr, text, (position[0] + x, position[1] + y),
+                    itc.font, itc.font_size, (0, 50, 100), 1, cv2.LINE_AA)
+    cv2.putText(fr, text, position,
+                itc.font, itc.font_size, (250, 200, 150), 1, cv2.LINE_AA)
 
 
-def label_frame(fr):
+def label_frame(fr: np.ndarray) -> np.ndarray:
 
     maintenant = datetime.datetime.now()
     timestamp = maintenant.strftime("%H:%M:%S")
-    timestamp = f"{timestamp}.{itc.count9}"
+    timestamp = f"{timestamp}.{itc.count9} "
     video_title = f" {sc.page_title}{itc.fps_value:6.2f} Hz"
-    y_bottom = frame_size_y - itc.size[1]
-    x_right = frame_size_x - 70
+    y_bottom = frame_size[1] - itc.size[1]
+    x_right = frame_size[0] - itc.get_size(timestamp)[0]
 
-    contrast(                      fr, video_title,      (0,                                  12), FONT_SIZE)
-    contrast(                      fr, timestamp,        (x_right,                            12), FONT_SIZE)
-    contrast(                      fr, itc.text,         (itc.x,                        y_bottom), FONT_SIZE)
-    if itc.x < 0: contrast(        fr, itc.text,         (frame_size_x + itc.x,         y_bottom), FONT_SIZE)
+    outline(                      fr, video_title,      (0,                                  12))
+    outline(                      fr, timestamp,        (x_right,                            12))
+    outline(                      fr, itc.text,         (itc.x,                        y_bottom))
+    if itc.x < 0: outline(        fr, itc.text,         (frame_size[0] + itc.x,         y_bottom))
     return fr
 
 
-def analyze_frame(fr):
+def analyze_frame(fr: np.ndarray) -> np.ndarray:
 
     gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
     sparrow = cascade[0].detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize=(30, 30))
 #    if len(sparrow) > 0:
 
-#        save_frame("positives")
+#        save_frame('+')
 #    else:
 
-#        save_frame("negatives")
+#        save_frame('-')
 
     spindex = 0
     for (x, y, w, h) in sparrow:
 
         cv2.rectangle(fr, (x, y), (x + w, y + h), (255, 255, 0), 1)
-        cv2.putText(fr, f"{spindex} {itc.wheel()}", (x, y + 12), simplex, 0.5, (255, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(fr, f"{spindex} {itc.wheel()}", (x, y + 12), itc.font, 0.5, (255, 255, 0), 1, cv2.LINE_AA)
         spindex += 1
     return fr
 
 
-def process_frame(fr):
+def process_frame(fr: np.ndarray) -> np.ndarray:
 
     analyzed_frame = analyze_frame(fr)
     labeled_frame = label_frame(analyzed_frame)
@@ -252,12 +274,17 @@ def read_stream():
     cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
     if not cap.isOpened():
 
-        print(f"... cannot capture {sc.video_device}")
+        server.logger.error(f"... cannot capture {sc.video_device}")
         itc.running = False
-    else:
-
-        print(f"... captured {sc.video_device}")
-        itc.running = True
+        return
+    server.logger.info(f"... captured {sc.video_device}")
+    itc.running = True
+    server.logger.info("keyboard commands:")
+    server.logger.info("[d] dump context")
+    server.logger.info("[D] dump data")
+    server.logger.info("[n] save a negative image")
+    server.logger.info("[p] save a positive image")
+    server.logger.info("[q] quit the application")
     read_count = time.perf_counter()
     while itc.running:
 
@@ -269,23 +296,21 @@ def read_stream():
         success, raw_frame = cap.read()
         if not success:
 
-            print(f"cannot read {sc.video_device}")
+            server.logger.error(f"cannot read {sc.video_device}")
             read_count = time.perf_counter()
             gevent.sleep(1)
             continue
-        raw_frame_resized = cv2.resize(raw_frame, (frame_size_x, frame_size_y))
+        raw_frame_resized = cv2.resize(raw_frame, frame_size)
         raw_frame_resized_copy = raw_frame_resized.copy()
         processed_frame = process_frame(raw_frame_resized_copy)
         with bs_lock:
 
             itc.raw = raw_frame_resized
             itc.frame = processed_frame # .copy()
-
-        if itc.new_frame():
-
-            save_frame("negatives", "random")
+        if itc.new_frame(): save_frame('-', 'random')
+        itc.fps_value = 1 / (time.perf_counter() - read_count)
+        itc.save_random = int(itc.fps_value * sc.save_rand_min * 60)
         read_count = time.perf_counter()
-
     cap.release()
 
 
@@ -317,7 +342,8 @@ def generation():
 
         _, jpeg = cv2.imencode('.jpg', last_frame, [cv2.IMWRITE_JPEG_QUALITY, sc.jpeg_quality])
 
-        itc.fps_value = 1 / (time.perf_counter() - gen_count)
+        #itc.fps_value = 1 / (time.perf_counter() - gen_count)
+        #itc.save_random = int(itc.fps_value * sc.save_rand_min * 60)
         gen_count = time.perf_counter()
 
         try:
@@ -364,9 +390,9 @@ def access_source_code():
 def index():
 
     audio_files = []
-    if os.path.exists(AUDIO_DIR):
+    if os.path.exists(sc.audio_dir):
 
-        list_dir = os.listdir(AUDIO_DIR)
+        list_dir = os.listdir(sc.audio_dir)
         audio_files =  [f for f in list_dir if f.endswith(".mp3")]
     else:
 
@@ -383,7 +409,7 @@ def index():
 def serve_audio(filename):
 
     itc.new_message(filename)
-    return send_from_directory(AUDIO_LOC, filename)
+    return send_from_directory('/static/audio', filename)
 
 
 @server.route('/sitemap.xml', methods=['GET'])
@@ -431,21 +457,14 @@ def before_request():
         ip_ban.block(ip)
         server.logger.warning(f"non-ascii request - save {ip} as banned and return error")
         return "non-ascii characters in request", 403
-
-#        return "you have been added to the black list", 403
-    # elif ip_ban.is_banned(client_ip):
-
-    #     server.logger.warning(f"{client_ip} is in the list of banned")
-    #     return "you are already in the black list", 403
     else:
 
-        with bs_lock:
-
-            client_set.add(ip)
+        with bs_lock: client_set.add(ip)
 
 
 @server.after_request
 def after_request(response): return response
+
 
 @server.route('/src')
 def get_text():
@@ -456,9 +475,7 @@ def get_text():
 @server.route('/client_counter')
 def get_clients():
 
-    with bs_lock:
-
-        return f"{len(client_set)}"
+    with bs_lock: return f"{len(client_set)}"
 
 
 def run_server():
@@ -469,27 +486,14 @@ def run_server():
 
 if __name__ == '__main__':
 
-    sc.dump()
     pool = Pool(sc.gevent_workers)
-
     timestamp = datetime.datetime.now().strftime("%H:%M:%S")
     server.logger.info(f"starting flask server at {timestamp}")
-
     pool.spawn(debug_wrapper, run_server)
     server.logger.info("starting stream")
-
     pool.spawn(debug_wrapper, read_stream)
     server.logger.info("starting keyboard listener")
-
     pool.spawn(debug_wrapper, keyboard_listener)
-    # server.logger.info(f"ban_count {ip_ban.ban_count} ban_seconds {ip_ban.ban_seconds}")
-    #server.logger.info(f"ip_ban_list ({len(ip_ban._ip_ban_list)})")
-    #server.logger.info("\nip_ban_list: ".join(ip_ban._ip_ban_list))
-    print("keyboard commands:")
-    print("[n] save a negative image")
-    print("[p] save a positive image")
-    print("[q] quit the application")
-
     while itc.running: gevent.sleep(0.5)
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     server.logger.warning("stop signal detected\n")
